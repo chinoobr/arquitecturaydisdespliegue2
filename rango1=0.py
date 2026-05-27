b1b08463da4fb81c23432fb3f8b13a812f7606cf
chinoobr <ticianoberon06@gmail.com>
2026-05-27 17:37:23 -0300

    agregar primer archivo

---
+++ rango1=0.py
@@ -0,0 +1,199 @@
+tribu_azul = []
+tribu_verde = []
+
+if(tribu_azul):
+
+    print("te toco la tribu azul")
+
+if(tribu_verde):
+
+    print("te toco la tribu verde")
+
+else:
+    ("te quedaste sin tribu")
+
+
+equipo_azul = []
+equipo_verde = []
+
+
+if(equipo_azul):
+
+    print("que actividad quieres elegir?: ")
+
+    print("futbol, hamball, voley, ajedrez, tejo")
+    
+   
+
+
+
+if(equipo_verde):
+    print("que actividad quieres elegir?: ")
+
+    print("futbol, hamball, voley, ajedrez, tejo")
+
+    
+
+
+
+else:
+    ("no hay mas actividades")
+
+
+
+
+arbitros = []
+
+if(arbitros):
+
+    print("que arbitro va a cada actividad?: ")
+
+
+else:
+    ("no hay arbitro seleccionado")
+
+actividades = []
+
+
+for list = actividades
+tribu_azul ** actividades
+
+for list = actividades
+tribu_verde ** actividades
+
+if("equipo_verde = equipo_azul"):
+
+    equipo_azul**actividades
+    
+    equipo_verde**actividades
+
+
+
+else:
+    ("error")
+
+
+
+list = actividades
+
+
+while True:
+    actividades = equipo_azul
+
+    actividades = equipo_verde
+
+    actividades = tribu_azul
+
+    actividades = tribu_verde
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
